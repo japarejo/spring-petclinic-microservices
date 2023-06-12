09e60cbbc16797ca7738a07164b09fa265c47929
José Antonio Parejo Maestre <japarejo@gmail.com>
2023-06-12 17:43:48 +0200

    Changed ports on microservices

--- microservices/hexagonal-microservice-chasis/src/main/java/org/springframework/samples/microservicechasis/infrastructure/adapters/output/persistence/mapper/ProductPersistenceMapper.java
+++ microservices/hexagonal-microservice-chasis/src/main/java/org/springframework/samples/microservicechasis/infrastructure/adapters/output/persistence/mapper/ProductPersistenceMapper.java
@@ -1,13 +1,14 @@
 package org.springframework.samples.microservicechasis.infrastructure.adapters.output.persistence.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.samples.microservicechasis.domain.model.Product;
 import org.springframework.samples.microservicechasis.infrastructure.adapters.configuration.MapperSpringConfig;
 import org.springframework.samples.microservicechasis.infrastructure.adapters.output.persistence.entity.ProductEntity;
 
 @Mapper(config = MapperSpringConfig.class )
 public interface ProductPersistenceMapper {
-
+	
 	Product toProduct(ProductEntity productEntity);
 
 	ProductEntity toProductEntity(Product product);
--- microservices/microservice-chasis/src/main/java/org/springframework/samples/microservicechasis/restcontrollers/ServiceInstanceRestController.java
+++ microservices/microservice-chasis/src/main/java/org/springframework/samples/microservicechasis/restcontrollers/ServiceInstanceRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ class ServiceInstanceRestController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
-	@RequestMapping("/service-instances/{applicationName}")
+	@GetMapping("/service-instances/{applicationName}")
 	public List<ServiceInstance> serviceInstancesByApplicationName(
 			@PathVariable String applicationName) {
 		return this.discoveryClient.getInstances(applicationName);
